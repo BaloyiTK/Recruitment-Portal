1e646ac52dfb304c4027f1f4039b1e51a5ab09d0
BaloyiTK <tiyanikevinbaloyi@gmail.com>
2024-05-15 11:18:44 +0200

    Update authentication functionality: login and registration services

--- services/auth/login/checkNotExistingUser.js
+++ services/auth/login/checkNotExistingUser.js
@@ -1,4 +1,4 @@
-import User from "../../../models/User.js";
+import User from "../../../models/user.js";
 
 const checkNotExistingUser = async (email) => {
   const userFound = await User.findOne({ email });
--- services/auth/register/checkExistingUser.js
+++ services/auth/register/checkExistingUser.js
@@ -1,4 +1,4 @@
-import User from "../../../models/User.js";
+import User from "../../../models/user.js";
 
 const checkExistingUser = async (email) => {
   const userFound = await User.findOne({ email });

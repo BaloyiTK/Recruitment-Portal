460b8a6b0186773187e9ea50e7c0e227b06485df
BaloyiTK <tiyanikevinbaloyi@gmail.com>
2024-05-19 23:06:25 +0200

    modified:   services/auth/register/registerUser.js

--- controllers/auth/logoutController.js
+++ controllers/auth/logoutController.js
@@ -1,13 +1,12 @@
-//registerController.js
+// controllers/registerController.js
 import asyncHandler from "express-async-handler";
-import logoutUser from "../../services/auth/logout/logoutUser.js";
 
 const logout = asyncHandler(async (req, res) => {
   try {
-   await logoutUser(res);
+    res.clearCookie("token");
     res.status(200).json({ message: "Logout successful" });
   } catch (error) {
-    return res.status(400).json({ message: error.message });
+    res.status(400).json({ message: error.message });
   }
 });
 

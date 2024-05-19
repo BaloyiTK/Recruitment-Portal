d9d570b48777e476e7b0d889a25f478c57f8ecc8
BaloyiTK <tiyanikevinbaloyi@gmail.com>
2024-05-19 23:10:57 +0200

    modified:   services/auth/register/registerUser.js

--- controllers/auth/logoutController.js
+++ controllers/auth/logoutController.js
@@ -2,12 +2,23 @@
 import asyncHandler from "express-async-handler";
 
 const logout = asyncHandler(async (req, res) => {
-  try {
-    res.clearCookie("token");
-    res.status(200).json({ message: "Logout successful" });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
+  const token = req.cookies.token; // Get token from cookies
+
+  if (!token) { // Check if token exists
+    return res.json(false); // Return false if token doesn't exist
   }
+
+  // Set the token in the cookies to an expired date to invalidate it
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    expires: new Date(),
+    path: "/",
+    sameSite: "none",
+  });
+
+  return res.status(200).json({ message: "logged out succsessfully" }); // Return success message after logging out
 });
 
 export default logout;
+

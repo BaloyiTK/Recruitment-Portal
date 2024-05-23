e122ff37fbfc20e11561d01d6eff242e5bb4f9b4
BaloyiTK <tiyanikevinbaloyi@gmail.com>
2024-05-23 14:40:06 +0200

    Implement OTP functionality in auth controller and add cache middleware

--- controllers/auth/verifyOTP.js
+++ controllers/auth/verifyOTP.js
@@ -2,61 +2,56 @@ import asyncHandler from "express-async-handler";
 import User from "../../models/user.js";
 import { generateAuthToken } from "../../utils/generateAuthToken.js";
 import setAuthCookie from "../../utils/setAuthCookie.js";
-import Memcached from "memcached"; // Import Memcached library
 
-// Create a Memcached client instance
-const memcached = new Memcached('localhost:11211');
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', err => console.log('Redis Client Error', err));
+
+await client.connect();
+
 
 const verifyOTP = asyncHandler(async (req, res) => {
   try {
     const { otp } = req.body;
 
-    // Check if user exists in cache
-    memcached.get(otp, async (err, cachedUser) => {
-      if (err) {
-        console.error(err);
-        // If there's an error with cache, proceed without caching
-      }
+    if (!otp) {
+      return res.status(400).json({ message: "Please enter opt" });
+    }
 
-      if (cachedUser) {
-        // If user is found in cache, use cached data
-        const token = generateAuthToken(cachedUser);
-        setAuthCookie(res, token);
-        return res.status(200).json({ message: "Email verified successfully" });
-      }
+    // If user is not found in cache, fetch from the database
+    const user = await User.findOne({ "otp.code": otp });
 
-      // If user is not found in cache, fetch from the database
-      const user = await User.findOne({ "otp.code": otp });
+    if (!user) {
+      return res.status(404).json({ message: "Invalid OTP" });
+    }
+    const userId = user.id.toString();
 
-      if (!user) {
-        return res.status(404).json({ message: "Invalid OTP" });
-      }
 
-      const otpExpiration = user.otp.expires;
-      const currentTime = new Date();
+    await client.set('"userId"', userId);
+    const value = await client.get('"userId"');
+    console.log(value);
 
-      if (currentTime > otpExpiration) {
-        user.otp = null;
-        await user.save();
-        return res.status(400).json({ message: "OTP has expired" });
-      }
 
-      user.isEmailVerified = true;
+
+    const otpExpiration = user.otp.expires;
+    const currentTime = new Date();
+
+    if (currentTime > otpExpiration) {
       user.otp = null;
       await user.save();
+      return res.status(400).json({ message: "OTP has expired" });
+    }
 
-      // Store user data in cache with the OTP as key
-      memcached.set( user, 3600, (cacheErr) => {
-        if (cacheErr) {
-          console.error(cacheErr);
-          // If there's an error caching, proceed without caching
-        }
-      });
+    user.isEmailVerified = true;
+    user.otp = null;
+    await user.save();
 
-      const token = generateAuthToken(user);
-      setAuthCookie(res, token);
-      res.status(200).json({ message: "Email verified successfully" });
-    });
+
+    const token = generateAuthToken(user);
+    setAuthCookie(res, token);
+    res.status(200).json({ message: "Email verified successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: "Internal server error" });

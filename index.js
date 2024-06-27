d892cb73675cd2c51cf171922517c918a186c1a5
BaloyiTK <tiyanikevinbaloyi@gmail.com>
2024-06-27 23:08:13 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 const uri = process.env.DATABASE_URI;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json()); // Middleware to parse JSON request bodies
